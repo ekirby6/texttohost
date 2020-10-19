4c8f2dc03d60178c7fa8ddacbd77c35703c886d7
ekirby6 <46833780+ekirby6@users.noreply.github.com>
2020-10-19 15:11:12 -0400

    completed v1 of planetmc alltime web scraping

--- planetminecraft/planetminecraft/spiders/planetmc_alltime.py
+++ planetminecraft/planetminecraft/spiders/planetmc_alltime.py
@@ -1,8 +1,8 @@
-import scrapy                # library used for web scraping
-from datetime import date    # used to get the date stamp of the scrape
+import scrapy  # library used for web scraping
+from datetime import date  # used to get the date stamp of the scrape
 
 
-def convert_to_int(str_num):   # used to convert the map stats numbers into numeric integer forms
+def convert_to_int(str_num):  # used to convert the map stats numbers into numeric integer forms
     if str_num[-1:] == 'k':  # check if the last digit is k
         return int(float(str_num[:-1]) * 1000)  # Remove the last digit with [:-1], then convert to integer
     elif str_num[-1:] == 'm':  # check if the last digit is m
@@ -12,7 +12,7 @@ def convert_to_int(str_num):   # used to convert the map stats numbers into nume
 
 
 class MapSpider(scrapy.Spider):
-    name = "pmc alltime maps"                 # use the name to call the web scraping in the terminal: scrapy crawl "_"
+    name = "pmc alltime maps"  # call web scraping in the terminal: scrapy crawl "pmc alltime maps" -o pmcalltime.json
 
     def start_requests(self):
         urls = [
@@ -22,57 +22,57 @@ class MapSpider(scrapy.Spider):
         # urls for most popular maps: all time
         # **need to change this to algorithm!
 
-        for url in urls:      # the urls we are scraping
+        for url in urls:  # the urls we are scraping
             yield scrapy.Request(url=url, callback=self.parse)  # parses the urls by attributes
 
     def parse(self, response):
-        page = response.url[-1]   # grabs the page number from the website url
+        page = response.url[-1]  # grabs the page number from the website url
         # [-1] gives the last character (the page number)
         filename = 'pmcalltimemaps-%s.html' % page  # need to name the files with different names so use the page number
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)  # logs that it was actually saved
 
-        r2 = response.css(".content")   # found using selector gadget, the center section. start big & get smaller
+        r2 = response.css(".content")  # found using selector gadget, the center section. start big & get smaller
 
-        # map_dict = {}     # creating empty dictionary
-
-        for map in r2.css(".r-info"):     # iterates through all of the maps in the selected section
+        for map in r2.css(".r-info"):  # iterates through all of the maps in the selected section
             map_title = map.css(".r-title::text").get()
             map_subtitle = map.css(".r-subtitle").css(".r-subject::text").get()
 
             # finding map author using the string methods
             str1 = map.css(".contributed").get()
             author_start = str1.find('load">') + 7  # start of author's name in the string
-            author_end = str1.find('</a>') - 1     # end of author's name in the string
+            author_end = str1.find('</a>') - 1  # end of author's name in the string
             map_author = str1[author_start:author_end]  # sliced substring including only the formatted author's name
 
             # map_description- planetmc doesn't have descriptions so disregard
             map_pageurl = response.url  # the main page url that the map was on, for records
-            map_descriptionurl = 'www.planetminecraft.com' + map.css(".r-info > a::attr('href')").get() # link from clicking-in to individual map details
+            map_descriptionurl = 'www.planetminecraft.com' + map.css(
+                ".r-info > a::attr('href')").get()  # link from clicking-in to individual map details
             # map_downloadurl - third party download issues, don't worry about for now
-            map_lastupdateddate = map.css(".timeago::text").get()    # date from the last update of the map
+            map_lastupdateddate = map.css(".timeago::text").get()  # date from the last update of the map
             map_dateaccessed = date.today().strftime("%m/%d/%Y")  # date of the scrape in format mm/dd/yyyy
             map_source = "planetminecraft all-time best"
 
             # map stats (get in string format, then convert to int
             str2 = map.css(".r-stats").get()
-            str_views = str2[str2.find('visibility')+23:str2.find('get_app')-33]  # count of map views
+            str_views = str2[str2.find('visibility') + 23:str2.find('get_app') - 33]  # count of map views
             map_views = convert_to_int(str_views)
             str_downloads = str2[str2.find('get_app') + 20:str2.find('chat_bubble') - 33]  # count of map downloads
             map_downloads = convert_to_int(str_downloads)
-            str_comments = str2[str2.find('chat_bubble')+24:str2.find('</span></div>')]    # count of map comments
+            str_comments = str2[str2.find('chat_bubble') + 24:str2.find('</span></div>')]  # count of map comments
             map_comments = convert_to_int(str_comments)
 
             # **include filter to put a null value ("NA") if a stat is missing??
 
-           # map_dict.update({map_title:{"Subtitle":map_subtitle}}) # stores all the data in the dict, searchable by subtitle
-        #     pass
-        # print(map_dict)   # write to csv first and then later json file**
+            # map_dict.update({map_title:{"Subtitle":map_subtitle}})
+            # stores all the data in the dict, searchable by subtitle. just need to make empty dict first
+
             yield {
                 'title': map_title,
                 'subtitle': map_subtitle,
                 'author': map_author,
+                'page': page,
                 'page URL': map_pageurl,
                 'map description URL': map_descriptionurl,
                 'date of last update': map_lastupdateddate,
@@ -83,4 +83,4 @@ class MapSpider(scrapy.Spider):
                 'comments': map_comments
             }
 
-        # work on NA stats and algorithm/dict/storage**
+        # work on NA stats and algorithm**
